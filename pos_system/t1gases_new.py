924779cd1fe7fc6d0ec7cc4ba44579f12a73c8b5
Mthokozisi Sapuwa <155919550+MthoeSaps@users.noreply.github.com>
2024-09-02 09:15:08 +0200

    Update t1gases_new.py

--- pos_system/t1gases_new.py
+++ pos_system/t1gases_new.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def calculate_gas_sold(amount_paid):
     gas_price = 1.90  # $1.80 per 1kg of gas
     gas_sold = amount_paid / gas_price
-    return gas_sol
+    return gas_sold
 
 def calculate_gas_sold_zar(amount_paid):
     gas_price = 40  # 20 ZAR per 1kg of gas
